92984a8904c371f6378661875e01ca673f01ed59
nermeen213 <nermeeen1734@gmail.com>
2023-11-12 23:11:16 +0200

    sec commit

--- js/main.js
+++ js/main.js
@@ -15,33 +15,24 @@ let todaymonth=document.getElementById('todayMonth')
 
 
 // 
-// let lastImg =document.getElementsByClassName('lastImg');
-// let degree =document.getElementsByClassName('degree');
-// let degreeSmall =document.getElementsByClassName('degreeSmall');
-// let lastText =document.getElementsByClassName('lastText');
+let lastImg =document.getElementsByClassName('lastImg');
+let degree =document.getElementsByClassName('degree');
+let degreeSmall =document.getElementsByClassName('degreeSmall');
+let lastText =document.getElementsByClassName('lastText');
+let nextDay =document.getElementsByClassName('nextday');
+console.log(nextDay);
 //***********************
 
 
-let lastImg =document.getElementById('twoImg');
-let degree =document.getElementById('twoDegree');
-let degreeSmall =document.getElementById('degreeSmall');
-let lastText =document.getElementById('twoText');
-let nextDay =document.getElementById('nextday');
 
-//***********************
-let threeImg =document.getElementById('threeImg');
-let threedegree =document.getElementById('threeDegree');
-let threedegreeSmall =document.getElementById('threeDegreeSmall');
-let threeText =document.getElementById('threeText');
-let nextDay2 =document.getElementById('nextday2');
 
 
 
 // **********API*************
 
 async function getApi(city){
-   let urlApi= await fetch(`https://api.weatherapi.com/v1/forecast.json?key=eba9a04f9ea5464081d10706232908&q=${city}&days-3`)
-   let respons =await urlApi.json();
+   let respons= await fetch(`https://api.weatherapi.com/v1/forecast.json?key=eba9a04f9ea5464081d10706232908&q=${city}&days=3`)
+    respons =await respons.json();
    return respons
 }
 
@@ -60,14 +51,14 @@ function currentweather(data){
     let todayDate = new Date()
     todayName.innerHTML=todayDate.toLocaleDateString('en-us',{weekday:'long'})
     todayNumber.innerHTML=todayDate.getDate()
-    todayMonth.innerHTML = todayDate.toLocaleDateString("en-US",{month:"long"})
+    todaymonth.innerHTML = todayDate.toLocaleDateString("en-US",{month:"long"})
 
     place.innerHTML=data.location.name;
     temperature.innerHTML=data.current.temp_c +`<sup>o</sup>C`;
     currentImg.setAttribute("src",'https:'+data.current.condition.icon);
     currenttext.innerHTML=data.current.condition.text
-    umbrella.innerHTML=data.current.humidity+'%';
-    wind.innerHTML=data.current.wind_kph+'km/h';
+    umbrella.innerHTML=data.current.humidity+`% <i class="fa-solid fa-umbrella fa-lg">`;
+    wind.innerHTML=data.current.wind_kph+`km/h <i class="fa-solid fa-wind fa-lg">`;
     windDirction.innerHTML='<i class="fa-regular fa-compass fa-lg"></i>'+data.current.wind_dir;
 
 
@@ -76,39 +67,27 @@ function currentweather(data){
 }
 // ****************************************************
 
-function displayNextData(data)
-{
-    let forecastData = data.forecast.forecastday[0]
-    let nextData= new Date(forecastData.date)
-    console.log(nextData);
-    nextDay.innerHTML = nextData.toLocaleDateString("en-US",{weekday:"long"})
-
-    degree.innerHTML=forecastData.day.maxtemp_c+'<sup>o</sup>C'
-    degreeSmall.innerHTML=forecastData.day.mintemp_c+'<sup>o</sup>C'
-    lastText.innerHTML=forecastData.day.condition.text
-    lastImg.setAttribute('src','https:'+forecastData.day.condition.icon)
 
 
-    
-    
-}
+function NextDay(data){
+  let forecastData = data.forecast.forecastday
+  
+  
+  
+  for(let i=0 ; i<2 ;i++){
+    let nextdata= new Date(forecastData[i+1].date)
 
-// ****************************************************
-function displayThree(data)
-{
-    let forecastData = data.forecast.forecastday[0]
-    let nextData= new Date(forecastData.date)
-    nextDay2.innerHTML = nextData.toLocaleDateString("en-US",{weekday:"long"})
+    nextDay[i].innerHTML = nextdata.toLocaleDateString("en-US",{weekday:"long"})
 
-
-    threedegree.innerHTML=forecastData.day.maxtemp_c+'<sup>o</sup>C'
-    threedegreeSmall.innerHTML=forecastData.day.mintemp_c+'<sup>o</sup>C'
-    threeText.innerHTML=forecastData.day.condition.text
-    threeImg.setAttribute('src','https:'+forecastData.day.condition.icon)
+    degree[i].innerHTML=forecastData[i+1].day.maxtemp_c+'<sup>o</sup>C'
+    degreeSmall[i].innerHTML=forecastData[i+1].day.mintemp_c+'<sup>o</sup>C'
+    lastText[i].innerHTML=forecastData[i+1].day.condition.text
+    lastImg[i].setAttribute('src','https:'+forecastData[i+1].day.condition.icon)
 
 
-    
-    
+
+  }
+
 }
 
 
@@ -117,14 +96,14 @@ function displayThree(data)
 // ****************************************************
 
 
-async function displayAPP(city="london"){
+async function displayAPP(city="cairo"){
   let currentdisplay= await getApi(city)
  
   if(!currentdisplay.error)
   {
     currentweather(currentdisplay)
-    displayNextData(currentdisplay)
-    displayThree(currentdisplay)
+    NextDay(currentdisplay)
+    
   }
 
 
